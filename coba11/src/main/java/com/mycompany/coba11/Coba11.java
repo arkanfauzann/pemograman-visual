64c75a67509e5892f5876748c2a043cb2ecae9c2
arkanfauzann <141114307+arkanfauzann@users.noreply.github.com>
2024-08-20 14:20:07 +0700

    Add files via upload

---
+++ coba11/src/main/java/com/mycompany/coba11/Coba11.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.coba11;
+
+/**
+ *
+ * @author M.F.R - R.G.I
+ */
+public class Coba11 {
+
+    public static void main(String[] args) {
+        percobaan coba=new percobaan();
+        coba.setVisible(true);
+    }
+}
